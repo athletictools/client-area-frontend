d9ca6010a9faa98a7e253ee6397f864455acc51c
John <john@Johns-MacBook-Pro.local>
2021-04-20 20:52:27 +0300

    Add card to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,52 @@
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import {
+    IonAvatar,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonContent,
+    IonHeader,
+    IonImg, IonItem, IonLabel, IonList, IonNote,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './HomePage.css';
+import React from "react";
 
 const HomePage: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Личный кабинет</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Личный кабинет</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Личный кабинет</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonCard>
+                    <IonCardContent>
+                        <IonList>
+                            <IonItem>
+                                <IonAvatar slot="start">
+                                    <img
+                                        src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y"/>
+                                </IonAvatar>
+                                <IonLabel>
+                                    <h3>Татаринцев Евгений Васильевич</h3>
+                                    <p>dolor sit amet</p>
+                                </IonLabel>
+                            </IonItem>
+                            <IonItem>
+                                <IonLabel>Баланс</IonLabel>
+                                <IonNote slot="end">1300р</IonNote>
+                            </IonItem>
+                        </IonList>
+                    </IonCardContent>
+                </IonCard>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default HomePage;
--- src/pages/contacts/ContactsPage.tsx
+++ src/pages/contacts/ContactsPage.tsx
@@ -14,7 +14,7 @@ import {
 import './ContactsPage.css';
 import React from "react";
 
-const LogoUrl = 'https://s94vla.storage.yandex.net/rdisk/42fbfa113d8053b8f00325b90d25a629e83ed45f7265a4f823dbf6ae462fb885/607c3b98/24jckCfIY3PagHSP7qbglz68tdBsIJ2Zn-Vgsr3qSFlVUPAMBxMwcY8Q3cBptPjB11DcUGx3E2Zgah_9XXbWrQ==?uid=0&filename=b7YosPbyXGA.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&fsize=30197&hid=be84860f182651f4e0d13b25c0e31d3a&media_type=image&tknv=v2&etag=dd3b7557b5a74885e72eff0532739d9b&rtoken=oEfPP922sH1m&force_default=no&ycrid=na-f0faaa198b0747003a68ca8e415ee99d-downloader21h&ts=5c03fa4541600&s=64896664ddd72a194a890e15e7d8fefaa3df28da4cc2ef769cb883f9f75000e2&pb=U2FsdGVkX19ni5khTuXUhipKo5bOEIuJ9fiDQ0DxWhJHWAy0zxT3qMevSIDJtkKk7wtLBphWm1UoBn3R4q5Fc7fGNA0nB0OnzKr-e9ZoMyc';
+const LogoUrl = 'https://via.placeholder.com/150x150';
 const ContactsPage: React.FC = () => {
   return (
     <IonPage>
